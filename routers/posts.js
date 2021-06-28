36da1349ee7a4685dae3531df6b34a01238dcfab
onanalee <69330162+onanalee@users.noreply.github.com>
2021-06-28 14:18:28 +0900

    Initial commit

---
+++ routers/posts.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const Posts = require("../schemas/posts")
+
+const router = express.Router();
+
+router.get("/list", async (req, res, next) => {
+  try {
+    const categories = await Posts.find({}, { _id: false, category: true })
+    res.json({ categories: categories });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+router.get("/posts/:contentId", async (req, res) => {
+  const { contentId } = req.params;
+  posts = await Posts.findOne({ contentId: contentId });
+  res.json({ posts: posts });
+});
+
+router.get("/posts", async (req, res, next) => {
+  try {
+    const { category } = req.query;
+    const posts = await Posts.find().sort('-contentId');
+    res.json({ posts: posts });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+router.post("/posts", async (req, res) => {
+  const { contentId, title, name, password, content } = req.body;
+  await Posts.create({ contentId, title, name, password, content });
+  res.send({ result: "success" });
+});
+
+router.post("/edit", async (req, res) => {
+  const { contentId, title, name, password, content } = req.body;
+  console.log('password', req.body.password);
+  // if(req.body.password === Posts.findOne({ password })){
+  //   await Posts.update({ contentId, title, name, password, content });
+  //   res.send({ result: "success" });
+  // } else {
+  //   console.log('wrong password!');
+  // }
+  
+});
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+// 몽고db 붕어빵 틀
+const connect = require('./schemas/index')
+connect();
+
+// 바디,json,media 데이터
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+app.use(express.static('public'));
+
+// 라우터
+const postsRouter = require("./routers/posts");
+app.use("/api", [postsRouter]);
+
+app.use((req, res, next) => {
+    console.log(req);
+    next();
+});
+
+// 템플릿 엔진
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+
+// 각종 url
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+})
+
+app.get('/write', (req, res) => {
+    res.render('write');
+})
+
+app.get('/read', (req, res) => {
+    res.render('read');
+})
+
+app.get('/list', (req, res) => {
+    res.render('list');
+})
+
+app.get('/edit', (req, res) => {
+    res.render('edit');
+})
+app.get('/allEntries', (req, res) => {
+    res.render('allEntries');
+})
+app.get('/viewEntry', (req, res) => {
+    res.render('viewEntry');
+})
+
+
+app.listen(port, () => {
+    console.log(`listening at http://localhost:${port}`)
+  })
+  
